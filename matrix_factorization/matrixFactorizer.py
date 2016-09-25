7efaf32dfd83a463abacf30cd23b6eb847df044b
ivicaobadic <obadic.ivica@gmail.com>
2016-09-25 11:42:12 +0200

    Added converting dictionary of all users and items into sparse matrix in 'Compressed Sparse Row' format

--- matrix_factorization/matrixFactorizer.py
+++ matrix_factorization/matrixFactorizer.py
@@ -1,7 +1,7 @@
 import dataset_parser.parser as datasetParser
 
 sparseMatrix = datasetParser.getSparseRatingsMatrix(
-    'C:\\Users\\ivicaobadic\\PycharmProjects\\DiplomaThesis\\dataset_parser\\dataset\\yelp_training_set_review.json'
+    'C:\\Users\\ivicaobadic\\PycharmProjects\\BachelorThesis\\dataset_parser\\dataset\\yelp_training_set_review.json'
     )
 
 print(sparseMatrix)
